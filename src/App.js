1bb4a501043c1442489f8dd871685e0224265d75
yerin512 <sosu0512@naver.com>
2021-10-22 18:16:25 +0900

    3.5 state를 사용할 때 주의사항

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
+import MyComponent from './MyComponent';
 
 function App() {
-  const number = undefined;
   return (
-    <div class="App">
-     {
-number && <>우왕</>
-     }
+    <div className="App">
+    <MyComponent numnum={'ㅇㅇ'} name="예린">리액트</MyComponent>
     </div>
   );
 }
 
 export default App;
+             
+
+// defaultProps 와 propTypes는 꼭 사용해야하나요?
+// 이 두 가지는 필수가 아니라 사용할 필요없음. 하지만 다른 개발자들과 함께 협업핮다면 해당 컴포넌트에 어떤 props가 필요한지
+// 쉽게 알 수 있어 개발 능률이 좋아질 것.
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>
+const MyComponent = ({name,numnum,children}) => {
+
+
+    // const nextObject = {...object, b: 2} // 사본을 만들어서 b 값만 덮어 쓰기.
+
+    const [object, setObject] = useState({a:1, b:2, c:3});
+
+const test = () => {
+setObject({
+    ...object,
+    b:88
+    }
+);
+console.log(object)
+
+}
+    return <div>안녕하세요, {name} 입니다.
+    <br/>
+    children 값은 {children}
+    좋아하는 건 {numnum}
+    <button onClick={()=>{test()}}/>
+    </div>
+
 }
 
+MyComponent.defaultProps = {
+    name:'기본'
+};
+
+MyComponent.propTypes = {
+    name : PropTypes.string,
+    numnum : PropTypes.string.isRequired
+
+};
+
 export default MyComponent;
-//이 코드는 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정함.
-
-
-
-//ES6의 화살표 함수.
-
-/*
-화살표 함수는 ES6 문법에서 함수를 표현하는 새로운 방식임. 
-그렇다고 해서 기존 function을 이용한 함수 선언 방식을 아예 대체하지는 않음.
-사용 용도가 다름. 이 문법은 주로 함수를 파라미터로 전달할 때 유용함.
-*/
-
-/*
-setTimout(function () {
-    console.log('hello, wolrd');
-}, 1000)
-
-
-setTimeout(()=>{
-
-    console.log('hello wold')
-
-}),1000);
-
-이 문법이 기존 function을 대체할 수 없는 이유는 용도가 다르기 때문임. 
-무엇보다 서로 가리키고 있는 this 값이 다름.
-
-*/
